84136bf5e2c9c77fde0bc9d0d424df73352b1bfe
Kaloyan Ganev <k.ganev@gmail.com>
2020-06-15 22:44:03 +0300

    Delete update_R.R

--- update_R.R
+++
@@ -1,4 +0,0 @@
-# installing/loading the package:
-if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
- 
-updateR(F, T, T, F, T, F, T) # install, move, update.package, quit R.
